02e1e7a6af62f3f63c7329348f0f0e818c175df9
jangrubin <wkdfnqls2465@gmail.com>
2023-03-23 16:14:24 +0900

    콜백지옥과 프로미스를  혼합시킨 연옥을 만들 뻔 했다

---
+++ callback/tryPromise.js
@@ -0,0 +1,20 @@
+// const loginUser = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (
+//       (id === "rubin" && password === "sexy") ||
+//       (id === "someone" && password === "kimochi")
+//     ) {
+//       resolve(user);
+//     }
+//     reject(console.log("로그인 실패"));
+//   }, 2000);
+//   setTimeout(() => {
+//     new Promise((user) => {
+//       if (user === "rubin") {
+//         console.log("오 루빈님 안녕하세요?");
+//       } else {
+//         console.log("뭐야 넌 루빈님이 아니잖아.");
+//       }
+//     });
+//   }, 2000);
+// });
